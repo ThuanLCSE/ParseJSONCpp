d293151058fd71aa1fbe93d782186505c216a2c1
THUANLCSE <thuanluong.aidev@gmail.com>
2019-04-26 22:51:46 +0200

    access object in JSON

--- main.cpp
+++ main.cpp
@@ -3,39 +3,51 @@
 #include "rapidjson/document.h"
 #include "rapidjson/writer.h"
 #include "rapidjson/stringbuffer.h"
-#include <iostream>
+#include <string.h>
 
 using namespace rapidjson;
+class Sensor {
+    public:
+        std::string ip, dump, toString;
+        double positionX, positionY;
+    public:
+        void print() {std::cout << "sensor of ip " << ip <<  std::endl; }
+};
 
 int main() {
     // 1. Parse a JSON string into DOM.
-    const char* json = "{\"project\":\"rapidjson\",\"stars\":10,\"arr\":[{\"a\":1},{\"a\":2}]}";
+    const char* json = "{\"sensors\":[{\"ip\":\"127.0.0.1\",\"positionX\":1,\"positionY\":2,\"dump\":\"dsa\",\"toString\":\"dsa\"}]}";
     Document document;
     document.Parse(json);
 
-    // 2. Modify it by DOM.
-    Value& s = document["stars"];
-    s.SetInt(s.GetInt() + 1);
-    Value& p = document["project"];
-    std::cout << p.GetString() << std::endl;
+    // 2. Get number of sensor connected
+    const Value& sensors = document["sensors"];
+    std::cout << sensors.Size() << std::endl;
 
-    //Array of Object
-    const Value& a = document["arr"];
-
-    for (SizeType i = 0; i < a.Size(); i++){
-        const Value& attribute = a[i];
-        assert(attribute.IsObject()); // each attribute is an object
-        for (Value::ConstMemberIterator itr2 = attribute.MemberBegin(); itr2 != attribute.MemberEnd(); ++itr2) {
-            std::cout << itr2->name.GetString() << " : " << itr2->value.GetInt() << std::endl;
+    // 3. Modify it by DOM.
+    for (SizeType i = 0; i < sensors.Size(); i++){
+        Sensor sen;
+        const Value& sensor = sensors[i];
+        for (Value::ConstMemberIterator iter = sensor.MemberBegin(); iter != sensor.MemberEnd(); ++iter) {
+            std::string attName = iter->name.GetString();
+            if (attName.compare("ip") == 0) {
+                sen.ip = iter->value.GetString();
+            }
+            if (attName.compare("positionX") == 0) {
+                std::cout << attName << " : " << iter->value.GetDouble() << std::endl;
+            }
+            if (attName.compare("positionY") == 0) {
+                std::cout << attName << " : " << iter->value.GetDouble() << std::endl;
+            }
+            if (attName.compare("dump") == 0) {
+                std::cout << attName << " : " << iter->value.GetString() << std::endl;
+            }
+            if (attName.compare("toString") == 0) {
+                std::cout << attName << " : " << iter->value.GetString() << std::endl;
+            }
         }
+        sen.print();
     }
 
-    // 3. Stringify the DOM
-    StringBuffer buffer;
-    Writer<StringBuffer> writer(buffer);
-    document.Accept(writer);
-
-    // Output {"project":"rapidjson","stars":11}
-    std::cout << buffer.GetString() << std::endl;
-    return 0;
+    return 1;
 }
